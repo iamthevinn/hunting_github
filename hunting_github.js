db913f779f453c58d3e3f79dd860ec4dce4990c3
Vince Carini <vincecarini@northwesternmutual.com>
2018-01-11 17:51:22 -0600

    Username working

--- hunting_github.js
+++ hunting_github.js
@@ -1,11 +1,8 @@
-//console.log("hello world")
-
-$.get("https://api.github.com/users/iamthevinn", displayName);
-// Notice that displayName is a function that takes in 1 parameter (this is the data that comes back from the API)
-function displayName(data) {
-    console.log(data["name"])
-}
-
 function clickToShowName() {
-    document.getElementById("nameText").appendChild(new_fruit);
+
+    $.get("https://api.github.com/users/iamthevinn", displayName);
+
+    function displayName(data) {
+        document.getElementById("nameText").innerHTML = data["name"];
+    }
 }
